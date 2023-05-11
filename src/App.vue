569446acd98445548a78bf5ed095ab89b4ac8077
githukelvin <kelvingithu019@gmail.com>
2023-05-11 16:00:05 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import css
+import TodoHeader from './components/TodoHeader.vue';
+import '/src/assets/CSS/main.css'
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <TodoHeader/>
+
+  <RouterView />
+</template>
+
+
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+import { RouterLink } from "vue-router";
+
+</script>
+
+<template>
+    <div>
+          <header>
+              <nav class="container">
+                  <div class="branding">
+                    <img src="../assets/images/Vue_Logo_Black.png" alt="image logo">
+                    <h1>Vue Todos</h1>
+                    </div> 
+                    <ul class="nav-routes">
+                       <RouterLink to="/">Home</RouterLink>
+                           <RouterLink to="/about">About</RouterLink>
+                    </ul>
+                </nav>
+          </header>
+    </div>
+</template>
+
